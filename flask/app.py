83dc926ad72cfb3f79de25c9ace40f285f7bd58e
borundev <borundev@gmail.com>
2020-12-24 20:48:06 +0100

    initial commit

---
+++ flask/app.py
@@ -0,0 +1,13 @@
+# flask_web/app.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/flask/')
+def hello_world():
+    return 'Hey, we have Flask in a Docker container!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
