62134ce603b544c013bad0fd5edaa6ff53a06a7f
borundev <borundev@gmail.com>
2020-12-25 22:28:00 +0100

    Removed unnecessary information

--- run.sh
+++ run.sh
@@ -2,6 +2,4 @@
 export NB_UID=$(id -u)
 export NB_GID=$(id -g)
 export NB_USER=$(whoami)
-export NOTEBOOKS_FOLDER='/home/borundev/Google Drive/machine_learning'
-export HOSTNAME='borundev-desktop'
 docker-compose up --build --force-recreate tensorflow_and_pytorch tensorboard
