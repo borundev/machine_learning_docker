6ce86b043321a50c30e18a8f2baf50fd1ec7861e
borundev <borundev@gmail.com>
2020-12-30 19:24:36 +0100

    Replace jupyter image with custom built one as the jupyter one has some logic that gets jupyter to write to /home/jovyan/ for note book secrets etc no matter who the user is.

--- flask/source_environment_variables.sh
+++ flask/source_environment_variables.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 export NB_UID=$(id -u)
 export NB_GID=$(id -g)
 export NB_USER=$(whoami)
-export HOSTNAME=$(hostname)
+if [ -z ${HOSTNAME+x} ]
+then 
+	export HOSTNAME=$(hostname)
+	echo "SETTING HOSTNAME = '$HOSTNAME'"
+else 
+	echo "HOSTNAME = '$HOSTNAME'"
+fi
 
 export FLASK_HTTP_USER=user
-# password is password
 export FLASK_HTTP_AUTH_PASS_ENC="\$apr1\$0rh0Tkr3\$ALeFxLzN7Q7m7fvaBFB8H0"
+
+if [ -z ${NOTEBOOKS_FOLDER+x} ]; then echo "NOTEBOOKS_FOLDER is unset"; else echo "NOTEBOOKS_FOLDER is set to '$NOTEBOOKS_FOLDER'"; fi
